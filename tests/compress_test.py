4e8815b8c709ac738a2797189c99c0e64d66e1c8
Aditya Kamath <akkamath@uw.edu>
2024-10-16 05:56:20 +0000

    Update compress test

--- tests/compress_test.py
+++ tests/compress_test.py
@@ -1,11 +1,35 @@
 import ibp
 import torch
 
-# Create a tensor
-sparse_tensor = torch.tensor([[4, 2, 5], [5, 4, 7], [6, 8, 13], [7, 16, 21]], dtype=torch.int64).pin_memory()
-print(sparse_tensor)
+def do_the_process(tensor):
+    # Preprocess to generate mask and bitval
+    mask, bitval = ibp.preprocess(tensor)
+    # Check compressed size
+    comp_sizes = ibp.get_compress_size(tensor, mask, bitval)
+    # Calculations
+    og_size = tensor.numel() * tensor.element_size()
+    comp_size = int(torch.sum(comp_sizes))
+    ratio = og_size / comp_size
+    print(f"OG {og_size} Compress: {comp_size} ({ratio:.2f}x)")
+
 # Turn on debug print messages
 ibp.print_debug(True)
-mask, bitval = ibp.preprocess(sparse_tensor)
-for i in range(mask.size(0)):
-    print("{:#04x} {:d}".format(mask[i], bitval[i]))
+
+# Create a tensor
+sp_tensor = torch.tensor([range(0, 100)] * 8, dtype=torch.int32).pin_memory()
+do_the_process(sp_tensor)
+
+sp_tensor = sp_tensor.view(dtype=torch.int64)
+do_the_process(sp_tensor)
+
+# Create a tensor
+dense_tensor = torch.randint(2147483647, [8, 100], dtype=torch.int32).pin_memory()
+do_the_process(dense_tensor)
+
+dense_tensor = dense_tensor.view(dtype=torch.int64)
+do_the_process(dense_tensor)
+
+semidense = torch.randint(2147483647 // 100, [8, 100], dtype=torch.int32).pin_memory()
+do_the_process(semidense)
+semidense = semidense.view(dtype=torch.int64)
+do_the_process(semidense)
