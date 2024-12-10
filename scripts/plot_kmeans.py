c2907914f5732c527848692cdde44335327bbf1e
Aditya Kamath <akkamath@uw.edu>
2024-12-10 10:17:17 +0000

    Update kmeans test

--- scripts/plot_kmeans.py
+++ scripts/plot_kmeans.py
@@ -8,8 +8,10 @@ comp = {}
 ctr = 0
 comp[ctr] = {}
 total_nodes = 0
+cur_centroids = 0
 def extract_thput_base(file_path):
     global ctr
+    global cur_centroids
     with open(file_path, 'r') as file:
         data = file.readlines()
     for line in data:
@@ -19,17 +21,17 @@ def extract_thput_base(file_path):
             total_nodes = float(match.group(1))
         match = re.match(r"Centroids ([0-9]+), compressed: ([0-9.]+)", line.strip())
         if match:
-            # Number of centroids * 2 [1 mask, 1 bitval] / total_nodes * 100 [for percent]
-            centroids = float(match.group(1)) * 2 / float(total_nodes) * 100
-            # Compressed - overhead of centroids
-            comp_perc = float(match.group(2)) * 100 - centroids
-            print(f"{centroids:.3f}\t{float(match.group(2)) * 100:.3f}")
-            if(centroids in comp[ctr]):
+            centroids = float(match.group(1))
+            # Compressed - Number of centroids * 2 [1 mask, 1 bitval] / total_nodes * 100 [for percent]
+            comp_perc = float(match.group(2)) * 100 - centroids * 2 / float(total_nodes) * 100
+            print(f"{int(centroids)}\t{comp_perc:.3f}\t{float(match.group(2)) * 100:.3f}")
+            if centroids < cur_centroids:
                 ctr += 1
                 comp[ctr] = {}
+            cur_centroids = centroids
             comp[ctr][centroids] = comp_perc
 
-labels = ["Uniform", "Normal", "Zipf"]
+labels = ["Asteroids.f32", "Reddit", "Uniform", "Normal"]
 
 def main():
     file = sys.argv[1]
@@ -37,14 +39,14 @@ def main():
 
     plt.figure(figsize=(8, 2.5))
     for i in comp.keys():
-        plt.plot(comp[i].keys(), comp[i].values(), '.-')
-    plt.xlabel('Cluster overhead (%)')  # Label the x-axis
+        plt.plot(comp[i].keys(), comp[i].values(), '.-', label=labels[i])
+    plt.xlabel('Clusters')  # Label the x-axis
     plt.ylabel('Net space saved (%)')  # Label the x-axis
-    #plt.xscale('symlog')
+    plt.xscale('log')
     #plt.yscale('symlog')
     plt.legend()
 
-    plt.savefig(sys.argv[2] + ".pdf", bbox_inches='tight')  # Save the chart to a file
+    plt.savefig(sys.argv[2] + ".png", bbox_inches='tight')  # Save the chart to a file
     
 
 if __name__ == "__main__":
--- tests/kmeans_asteroid.py
+++ tests/kmeans_asteroid.py
@@ -8,7 +8,7 @@ def compress_kmeans(orig_tensor, print_out = False):
     comp_tensor = orig_tensor.detach().clone().pin_memory()
 
     centroids = 1
-    while centroids < comp_tensor.shape[0] * 0.2:
+    while centroids < comp_tensor.shape[0] * 0.1:
         masks,vals,clusterids = ibp.preprocess_kmeans(comp_tensor, centroids, None)
         centroids *= 2
     torch.cuda.synchronize()
@@ -18,17 +18,35 @@ DATASET_FOLDER = "./dlrm_feats"
 if len(sys.argv) > 1:
     DATASET_FOLDER = sys.argv[1]
 
-files = [DATASET_FOLDER+'/asteroid.f32']
-
 ibp.print_debug(True)
 
+print("Asteroid")
+file = DATASET_FOLDER+'/asteroid.f32'
 # PREPROCESSING AND SETTING UP DATA STRUCTURES
-sizes = []
-for file in files:
-    weights = np.memmap(file, dtype='float32', mode='r')
-    tensor = torch.from_numpy(weights).pin_memory()
-    tensor = tensor.reshape([tensor.shape[0], 1])
-    sizes.append(tensor.shape[0])
-    print(tensor.shape)
-    compress_kmeans(tensor)
-sizes = torch.tensor(sizes)
+weights = np.memmap(file, dtype='float32', mode='r')
+tensor = torch.from_numpy(weights).pin_memory()
+tensor = tensor.reshape([tensor.shape[0] // 500, 500])
+print(tensor.shape)
+compress_kmeans(tensor)
+
+print("Reddit")
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../../../training_backend/")
+import load_graph as ld
+g, features, labels, training_ids, validation_ids, testing_ids = ld.load("../../../dataset/", "reddit")
+compress_kmeans(features)
+
+INT_MAX = 2147483647
+NUM_VEC_SETS = 250000
+VEC_SIZE = 500
+
+# Uniform distribution
+print("Uniform")
+random_vec = torch.randint(INT_MAX, [NUM_VEC_SETS, VEC_SIZE], dtype=torch.int32).pin_memory()
+compress_kmeans(random_vec)
+
+# Normal distribution
+print("Normal")
+random_vec = torch.normal(float(0), float(INT_MAX / 3), size=(NUM_VEC_SETS, VEC_SIZE)).type(torch.int32).pin_memory()
+compress_kmeans(random_vec)
