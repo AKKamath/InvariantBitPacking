a75211b7328f2dbb415f5ad769665751acfb4d65
Aditya K Kamath <a_kamath@hotmail.com>
2026-02-21 23:16:07 +0000

    Comment unused code in DLRM and garbage collect frequently to try to avoid OOM

--- tests/dlrm_comp_merged.py
+++ tests/dlrm_comp_merged.py
@@ -5,6 +5,7 @@ from dgl.utils import gather_pinned_tensor_rows
 import numpy as np
 from colossalai.legacy.nn.parallel.layers import CachedEmbeddingBag
 import ibp
+import gc
 
 ITERS = 1000
 TABLES = 26
@@ -32,6 +33,7 @@ comp_runtime = {}
 base_runtime = {}
 T_SIZES = [128, 256, 512]
 for T_SIZE in T_SIZES:
+    gc.collect()
     comp_transfer[T_SIZE] = {}
     base_transfer[T_SIZE] = {}
     comp_runtime[T_SIZE] = {}
@@ -52,6 +54,7 @@ for T_SIZE in T_SIZES:
     TABLES = sizes.shape[0]
 
     table_weights = torch.empty([torch.sum(sizes), T_SIZE]).pin_memory()
+    gc.collect()
 
     # Make sizes a cumulative sum (inclusive scan)
     sizes = torch.cumsum(sizes, 0)
@@ -61,10 +64,11 @@ for T_SIZE in T_SIZES:
             table_weights[0 : sizes[i]] = tensor
         else:
             table_weights[sizes[i - 1] : sizes[i]] = tensor
+    gc.collect()
     # Increase cache ratio from default 0.01 if batch size is too big
     cache_ratio = min(0.01, max(BATCHES) * TABLES / sizes[-1])
     comp_emb_bags = CachedEmbeddingBag(table_weights.shape[0], table_weights.shape[1], cache_ratio=0.01, _weight=table_weights.view(torch.float64), comp=True, comp_imp=-1).to("cuda")
-    fake_emb_bags = CachedEmbeddingBag(table_weights.shape[0], table_weights.shape[1], cache_ratio=0.01,_weight=table_weights, comp=True, comp_imp=3).to("cuda")
+    #fake_emb_bags = CachedEmbeddingBag(table_weights.shape[0], table_weights.shape[1], cache_ratio=0.01,_weight=table_weights, comp=True, comp_imp=3).to("cuda")
     def_emb_bags  = CachedEmbeddingBag(table_weights.shape[0], table_weights.shape[1], cache_ratio=0.01,_weight=table_weights, comp=False).to("cuda")
 
     def compress(orig_tensor, print_out = False):
@@ -96,6 +100,7 @@ for T_SIZE in T_SIZES:
         return comp_tensor, mask, bitval, bitmask, comp_size, comp_sizes
 
     comp_tensor, mask, bitval, bitmask, comp_size, comp_arr = compress(table_weights.view(torch.int64))
+    gc.collect()
 
     bitmask_zero = bitmask.clone()
     bitmask_zero = bitmask_zero.zero_()
@@ -119,7 +124,7 @@ for T_SIZE in T_SIZES:
         # Embedding bag time
         return_val = comp_emb_bags(indices_cuda, offsets=offsets_cuda)
         return_val2 = def_emb_bags(indices_cuda, offsets=offsets_cuda)
-        return_val3 = fake_emb_bags(indices_cuda, offsets=offsets_cuda)
+        #return_val3 = fake_emb_bags(indices_cuda, offsets=offsets_cuda)
         torch.cuda.synchronize()
 
         tran_time = 0
@@ -132,7 +137,7 @@ for T_SIZE in T_SIZES:
         ctrs = torch.zeros([TABLES])
         comp_emb_bags.cache_weight_mgr._reset_comm_stats()
         def_emb_bags.cache_weight_mgr._reset_comm_stats()
-        fake_emb_bags.cache_weight_mgr._reset_comm_stats()
+        #fake_emb_bags.cache_weight_mgr._reset_comm_stats()
 
         # Construct indices
         # Get offset for each individual table and add to index value
@@ -160,6 +165,7 @@ for T_SIZE in T_SIZES:
         #
 
         # DGL data transfer
+        '''
         torch.cuda.synchronize()
         start = time.time_ns()
         for iter in range(ITERS):
@@ -167,6 +173,7 @@ for T_SIZE in T_SIZES:
         torch.cuda.synchronize()
         end = time.time_ns()
         tran2_time += end - start
+        '''
         '''
         if not(torch.equal(decomp_tensor1, tensor_tran2)):
             print("IBP mismatch")
@@ -253,6 +260,7 @@ for T_SIZE in T_SIZES:
             '''
 
             # Embedding bag time
+            '''
             torch.cuda.synchronize()
             start = time.time_ns()
             return_val2 = fake_emb_bags(indices_cuda, offsets=offsets_cuda)
@@ -260,6 +268,7 @@ for T_SIZE in T_SIZES:
             end = time.time_ns()
             bag3_time += end - start
             return_val2_cuda = return_val2.to("cuda")
+            '''
             '''
             if not(torch.equal(tensor_tran2, return_val2_cuda)):
                 print("Bag2 mismatch")
@@ -271,7 +280,7 @@ for T_SIZE in T_SIZES:
 
         print(f"Batch: {BATCH}")
         #print(f"Transfer (index + copy) time: {tran_time / ITERS / 1e6:.3f} ms")
-        print(f"Transfer (GPU copy) time: {tran2_time / ITERS / 1e6:.3f} ms")
+        #print(f"Transfer (GPU copy) time: {tran2_time / ITERS / 1e6:.3f} ms")
         print(f"IBP time: {ibp_time / ITERS / 1e6:.3f} ms")
         print(f"IBP2 time: {ibp2_time / ITERS / 1e6:.3f} ms")
         print(f"Cache_Emb_bag time: {bag_time / ITERS / 1e6:.3f} ms")
@@ -296,18 +305,28 @@ for T_SIZE in T_SIZES:
         myKeys.sort()
         sd = {i: d[i] for i in myKeys}
         print(sd)
-
+        '''
         d = fake_emb_bags.cache_weight_mgr._elapsed_dict
         myKeys = list(d.keys())
         myKeys.sort()
         sd = {i: d[i] for i in myKeys}
         print(sd)
+        '''
+    gc.collect()
 
 for BATCH in BATCHES:
     print(f"{BATCH}\tRuntime speedup\tTransfer speedup\tcomp_runtime\tbase_runtime\tcomp_transfer\tbase_transfer")
     for T_SIZE in T_SIZES:
         run_sp = base_runtime[T_SIZE][BATCH] / comp_runtime[T_SIZE][BATCH]
         transfer_sp = base_transfer[T_SIZE][BATCH] / comp_transfer[T_SIZE][BATCH]
-        print(f"{T_SIZE * 4}\t{run_sp:.4f}\t{transfer_sp:.4f}\t{comp_runtime[T_SIZE][BATCH]:.4f}\t{base_runtime[T_SIZE][BATCH]:.4f}\t" +
+        print(f"{T_SIZE * 4}B\t{run_sp:.4f}\t{transfer_sp:.4f}\t{comp_runtime[T_SIZE][BATCH]:.4f}\t{base_runtime[T_SIZE][BATCH]:.4f}\t" +
             f"{comp_transfer[T_SIZE][BATCH]:.4f}\t{base_transfer[T_SIZE][BATCH]:.4f}")
+    print()
+
+for BATCH in BATCHES:
+    header_parts = [f'{T_SIZE * 4}B speedup' for T_SIZE in T_SIZES]
+    print(f"Batch {BATCH}\t{chr(9).join(header_parts)}")
+    for T_SIZE in T_SIZES:
+        run_sp = base_runtime[T_SIZE][BATCH] / comp_runtime[T_SIZE][BATCH]
+        print(f"\t{run_sp:.4f}", end="")
     print()
